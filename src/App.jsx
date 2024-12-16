942eaa92e9dac2721e3fcc8018daa80108d17a2f
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2024-12-16 16:56:47 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter} from "react-router"
+import { IconName } from "react-icons/ci";
+
+const App = () => {
+  return (
+    <div>
+     <BrowserRouter>
+     <CiBatteryFull />
+     </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
